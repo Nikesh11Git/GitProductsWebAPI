af3c66a319efe95029a6874654813eb297f67ce8
Nikesh11Git <61278788+Nikesh11Git@users.noreply.github.com>
2020-02-22 15:05:56 +0530

    P Added

--- ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/Controllers/ProductsController.cs
@@ -14,8 +14,9 @@ namespace ProductsApp.Controllers
         {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-			new Product { Id = 4, Name = "Fan", Category = "Hardware", Price = 20M }
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+			new Product { Id = 4, Name = "Fan", Category = "Hardware", Price = 20M },
+			new Product { Id = 5, Name = "Fan2", Category = "Hardware", Price = 20M }
         };
 
         public IEnumerable<Product> GetAllProducts()
